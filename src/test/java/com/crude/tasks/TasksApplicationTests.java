0b4f6d785eb6ac1ed3404886ae8e193317b850d8
Adam Podlaski <podlaski83@gmail.com>
2023-10-24 23:46:15 +0200

    initial commit

---
+++ src/test/java/com/crude/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crude.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
