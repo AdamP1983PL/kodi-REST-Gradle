4aa4761a0505b6720fcea65a7470e2d2008f2d73
Adam Podlaski <podlaski83@gmail.com>
2023-11-13 23:01:19 +0100

    additional file commit

--- src/main/java/com/crude/tasks/controller/TaskController.java
+++ src/main/java/com/crude/tasks/controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
@@ -20,25 +21,15 @@ public class TaskController {
     private final TaskMapper taskMapper;
 
     @GetMapping
-//    public List<TaskDto> getTasks() {
-//        List<Task> tasks = service.getAllTasks();
-//        return taskMapper.mapToTaskDtoList(tasks);
-//    }
     public ResponseEntity<List<TaskDto>> getTasks() {
         List<Task> tasks = service.getAllTasks();
         return ResponseEntity.ok(taskMapper.mapToTaskDtoList(tasks));
     }
 
-
     @GetMapping(value = "{taskId}")
     public TaskDto getTask(@PathVariable("taskId") Long taskId) {
         return taskMapper.mapToTaskDto(service.getTask(taskId));
     }
-
-//    @GetMapping(value = "/optional/{taskId}")
-//    public TaskDto getOptionalTask(@PathVariable(name = "taskId") Long taskId) throws TaskNotFoundException{
-//        return taskMapper.mapToTaskDto(service.getOptionalTask(taskId).orElseThrow(TaskNotFoundException::new));
-//    }
 
     @GetMapping(value = "/re/{taskId}")
     public ResponseEntity<TaskDto> getTaskRE(@PathVariable(name = "taskId") Long taskId) throws TaskNotFoundException {
@@ -46,22 +37,11 @@ public class TaskController {
     }
 
 
-//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void createTask(@RequestBody TaskDto taskDto) {
-//        Task task = taskMapper.mapToTask(taskDto);
-//        service.saveTask(task);
-//    }
-
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createTask(@RequestBody TaskDto taskDto) {
         service.saveTask(taskMapper.mapToTask(taskDto));
         return ResponseEntity.ok().build();
     }
-
-//    @PutMapping
-//    public TaskDto updateTask(TaskDto taskDto) {
-//        return new TaskDto(1L, "Edited test title", "Test content");
-//    }
 
     @PutMapping
     public ResponseEntity<TaskDto> updateTask(@RequestBody TaskDto taskDto) {
--- src/main/java/com/crude/tasks/domain/Task.java
+++ src/main/java/com/crude/tasks/domain/Task.java
@@ -1,14 +1,10 @@
 package com.crude.tasks.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-import javax.persistence.*;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
 @Entity(name = "tasks")
 public class Task {
     @Id
@@ -20,4 +16,37 @@ public class Task {
 
     @Column(name = "description")
     private String content;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
